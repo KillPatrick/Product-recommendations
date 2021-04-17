f8f3a711f6ff2f25667c107a6c828f72ced969c6
KillPatrick <g.brusokas@gmail.com>
2021-04-17 20:18:47 +0300

    models-migrations-seeders

---
+++ database/factories/WeatherConditionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\WeatherCondition;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WeatherConditionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = WeatherCondition::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
